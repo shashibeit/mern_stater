3de4c0589cc371c1986a4ba852f9e998106d9c6b
Shashikant <shashibeit@gmail.com>
2018-10-31 23:27:09 +0530

    psuh

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-//const items=require("./routes/api/items");
+
+const singin=require("./routes/api/signin");
 
 const app = express();
 
@@ -16,8 +17,9 @@ mongoose.connect(db)
     .then(() => console.log("MongoDB Connected..."))
     .catch(err => console.log(err));
 
-//app.use("/api/items",items);
+    
+app.use("/",singin);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => (console.log(`Server started at ${port}`)));
+app.listen(port, () => (console.log(`Server Started at ${port}`)));
